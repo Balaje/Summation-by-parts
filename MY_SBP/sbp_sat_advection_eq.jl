7b3bb486cee57d564e08cd77bd13bb02f69c713f
Balaje K <balaje6@gmail.com>
2023-05-19 11:01:40 +0200

    Working SBP Method

--- MY_SBP/sbp_sat_advection_eq.jl
+++ MY_SBP/sbp_sat_advection_eq.jl
@@ -14,14 +14,14 @@ using StaticArrays
 using Plots
 
 # Define the problem
-a = 1.0
-c = 2.0
-ϵ = 0.1
-w = sqrt(c^2 - a^2)/(2ϵ)
-b = (c-a)/(2ϵ)
+const a = 1.0
+const c = 2.0
+const ϵ = 0.1
+const w = sqrt(c^2 - a^2)/(2ϵ)
+const b = (c-a)/(2ϵ)
 # Mixed boundary condition on x=0, Neumann boundary cndition on x=1
-α = 1
-β = 0
+const α = 1
+const β = 0
 u(x,t) = sin(w*(x-c*t))*exp(-b*x)
 f(x) = u(x,0)
 g₀(t) = (b-α)*sin(w*c*t) + w*cos(w*c*t)
@@ -35,6 +35,7 @@ Gets the scheme with second order accuracy at the boundary.
 function SBP(m::Int64)  
   # H discrete norm matrix
   h = 1/(m-1)
+  # h = 1
   H = spdiagm(ones(m))
   H[1:4, 1:4] = spdiagm([17/48, 59/48, 43/48, 49/48])
   H[m-3:m, m-3:m] = rotr90(spdiagm([17/48, 59/48, 43/48, 49/48]), 2)
@@ -76,7 +77,7 @@ function SBP(m::Int64)
   # Compatible second derivate D2c
   M = BS-H*D2
   S = BS
-  #S[1,:] = -S[1,:]
+  S[1,:] = -S[1,:]
   for i=2:size(S,1)-1
     S[i,i] = 0;    
   end
@@ -104,50 +105,50 @@ end
 """
 The RHS function for the Runge Kutta Iteration
 """
-function f!(res::Vector{T}, t::Float64, v::Vector{T}; kwargs) where T <: Number
-  coeffs, sbp, τ₀₁, ics = args
+function f(t::Float64, v::AbstractVector{T}, kwargs) where T <: Number  
+  coeffs, sbp, τ₀₁, ics = kwargs
   a,ϵ,α,β = coeffs
   τ₀, τ₁ = τ₀₁
   g₀, g₁ = ics
   HHinv, D1, D2s, S, unit_vecs = sbp
-  Hinv, = HHinv
+  _, Hinv = HHinv  
   E₀, Eₙ, e₀, eₙ, Id = unit_vecs
   D2, _ = D2s
-  copyto!(res, -a*D1*v + ϵ*D2*v - τ₀*(Hinv*(E₀*(α*Id+S)*v - e₀*g₀(t))) - τ₁*(Hinv*(Eₙ*(β*Id+S)*v - eₙ*g₁(t))))
-  res
+  g0 = g₀(t)
+  g1 = g₁(t)
+  -a*D1*v + ϵ*D2*v - τ₀*(Hinv*(E₀*(α*Id+S)*v - e₀*g0)) - τ₁*(Hinv*(Eₙ*(β*Id+S)*v - eₙ*g1))
 end
 
 """
 The Runge Kutta scheme
 """
-function RK4!(res::Vector{T}, Δt::Float64, t::Float64, u::Vector{T}; kwargs) where T<:Number
-  k₁ = f!(res, t, u; kwargs=kwargs)
-  k₂ = f!(res, t + 0.5*Δt, u + 0.5*Δt*k₁; kwargs=kwargs)
-  k₃ = f!(res, t + 0.5*Δt, u + 0.5*Δt*k₂; kwargs=kwargs)
-  k₄ = f!(res, t + Δt, u + Δt*k₃; kwargs=kwargs)
-  copyto!(res, (u + (Δt)/6*(k₁ + 2k₂ + 2k₃ + k₄)))  
-  res
+function RK4!(res::Vector{T}, Δt::Float64, t::Float64, u::Vector{T}, kwargs) where T<:Number
+  k₁ = f(t, u, kwargs)
+  k₂ = f(t + 0.5*Δt, u + 0.5*Δt*k₁, kwargs)
+  k₃ = f(t + 0.5*Δt, u + 0.5*Δt*k₂, kwargs)
+  k₄ = f(t + Δt, u + Δt*k₃, kwargs)    
+  res .= (u + (Δt)/6*(k₁ + 2k₂ + 2k₃ + k₄))  
 end
 
 # Discretization parameters
-N = 11
+N = 51
 x = LinRange(0,1,N)
 sbp = SBP(N);
 τ₀ = -ϵ
 τ₁ = ϵ
 args = (a,ϵ,α,β), sbp, (τ₀, τ₁), (g₀, g₁)
 tf = 1.0
-Δt = 0.1*(tf/(N-1))^2
+Δt = 1e-3
 ntime = ceil(Int64,tf/Δt)
 plt = plot()
 let
   u₀ = f.(x)
-  global u₁ = zero(u₀)
+  global u₁ = zero(u₀)  
   t = 0.0
   for i=1:ntime
-    u₀ = RK4!(u₁, Δt, t, u₀; kwargs=args)
+    u₀ = RK4!(u₁, Δt, t, u₀, args)    
     t = t+Δt
   end
-  plot!(plt, x, u.(x,tf), lc=:red, lw=1, ls=:dash)
-  plot!(plt, x, u₁, lc=:blue, lw=2)
+  plot!(plt, x, u.(x,tf), lc=:red, lw=2, ls=:dash, label="Exact solution")
+  plot!(plt, x, u₁, lc=:blue, lw=0.5, label="Approximate Solution")
 end
